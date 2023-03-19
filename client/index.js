deb2be6c23c6dda5bfdb91cd0cdb863365e9d028
eduairet <hola@eduairet.com>
2023-03-18 23:50:19 -0600

    Monorepo

--- client/index.js
+++ client/index.js
@@ -1,17 +1,18 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+import axios from 'axios';
+import niceList from '../utils/niceList.json';
+import MerkleTree from '../utils/MerkleTree';
 
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+    // TODO: how do we prove to the server we're on the nice list?
 
-  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
-  });
+    const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+        // TODO: add request body parameters here!
+    });
 
-  console.log({ gift });
+    console.log({ gift });
+    document.getElementById('name').textContent = gift;
 }
 
-main();
+document.getElementById('check').addEventListener('click', main);
--- server/index.js
+++ server/index.js
@@ -2,28 +2,31 @@ const express = require('express');
 const verifyProof = require('../utils/verifyProof');
 
 const port = 1225;
+const parent = require('path').resolve(__dirname, '..');
 
 const app = express();
 app.use(express.json());
+
+// Frontend
+app.use(express.static(parent + '/client'));
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
 const MERKLE_ROOT = '';
 
 app.post('/gift', (req, res) => {
-  // grab the parameters from the front-end here
-  const body = req.body;
+    // grab the parameters from the front-end here
+    const body = req.body;
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
-    res.send("You got a toy robot!");
-  }
-  else {
-    res.send("You are not on the list :(");
-  }
+    // TODO: prove that a name is in the list
+    const isInTheList = false;
+    if (isInTheList) {
+        res.send('You got a toy robot!');
+    } else {
+        res.send('You are not on the list :(');
+    }
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}!`);
+    console.log(`Listening on port ${port}!`);
 });
